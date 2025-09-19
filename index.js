7b038c3cfef2a1c272e4d366ca7c2c3c120368fb
Eddy E <v-ennorom@microsoft.com>
2025-09-19 16:24:42 -0700

    Added name path to server index file

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello, Express!');
 });
 
+app.get('/name', (req, res) => {
+  res.send('This is the name path');
+});
+
+
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
 });
